46daa078252c4fc446616c8d0ed7774b64d14fad
Donovan <djohn304@charlotte.edu>
2025-10-21 22:58:20 -0400

    Added Account Repo

---
+++ AccountRepository.cs
@@ -0,0 +1,22 @@
+namespace Lab2Recommendations;
+
+public class AccountRepository: IAccountRepository
+{
+    
+    public int Count => 0;
+    
+    public Member GetOrAdd(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Member GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Member> All()
+    {
+        throw new NotImplementedException();
+    }
+}
