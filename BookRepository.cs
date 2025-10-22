19edf05109c99b627333eb6cdb01af7de184b391
Kyrin Dukes <kdukes3@uncc.edu>
2025-10-21 22:45:52 -0400

    Book class changes and added BookRepository

---
+++ BookRepository.cs
@@ -0,0 +1,27 @@
+namespace Lab2Recommendations;
+
+public class BookRepository : IBookRepository
+{
+    private readonly List<Book> _books = new List<Book>();
+    public int Count => _books.Count;
+
+    public Book Add(Book b)
+    {
+        _books.Add(b); return b; 
+    }
+
+    public Book GetByIsbn(int isbn)
+    {
+        for (int i = 0; i < _books.Count; i++)
+        {
+            if (_books[i].Isbn == isbn)
+            {
+
+
+                return _books[i];
+                return null;
+            }
+        }
+
+        public List<Book> All() => _books;
+    }
--- IBook.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab2Recommendations;
-
-public interface IBook
-{
-    public void Book(string isbn, string title, string author, string year);
-    public Book GetByIsbn(int isbn);
-    //public List<Book> GetByIsbn(int isbn);
-}
