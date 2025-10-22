850ddbee20a00394a25c494dc3f47afaaf4f2d4f
Donovan <djohn304@charlotte.edu>
2025-10-22 18:41:00 -0400

    Updated Memory Repository and added Rating & Rating service

--- IRatingRepository.cs
+++ IRatingRepository.cs
@@ -4,5 +4,5 @@ public interface IRatingRepository
 {
     int Get(int memberId, int isbn);
     void Set(int memberId, int isbn, int value);
-    List<(int isbn,int value)> ForMember(int memberId);
+    List<Rating> GetAll();
 }
