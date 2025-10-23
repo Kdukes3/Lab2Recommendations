5b11870123ca41034751e1ed058afc7effc87a4c
Cameron Dingle <minorwinter29@gmail.com>
2025-10-22 20:33:55 -0400

    Implemented Menu enums

---
+++ LoggedInMenuOption.cs
@@ -0,0 +1,13 @@
+namespace Lab2Recommendations
+{
+    public enum LoggedInMenuOption
+    {
+        None = 0,
+        AddMember = 1,
+        AddBook = 2,
+        RateBook = 3,
+        ViewRatings = 4,
+        SeeRecommendations = 5,
+        Logout = 6
+    }
+}
---
+++ MainMenuOption.cs
@@ -0,0 +1,11 @@
+namespace Lab2Recommendations
+{
+    public enum MainMenuOption
+    {
+        None = 0,
+        AddMember = 1,
+        AddBook = 2,
+        Login = 3,
+        Quit = 4
+    }
+}
