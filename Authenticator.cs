66e795e64b5ae699f8853a6598a52cb66f131809
Cameron Dingle <cdingle1@charlotte.edu>
2025-10-23 04:05:13 -0400

    Updated Authenticator.cs and IAuthenticator.cs to allow messages to pass through

--- Authenticator.cs
+++ Authenticator.cs
@@ -4,53 +4,44 @@ public class Authenticator : IAuthenticator
 {
     private readonly IAccountRepository _accounts;
     private int _currentId = 0; // 0 means not logged in
-    public string message;
+    private string _message = "";
 
-    public Authenticator(IAccountRepository accounts)
-    {
-        _accounts = accounts;
-    }
+    public Authenticator(IAccountRepository accounts) => _accounts = accounts;
 
     public int CurrentId => _currentId;
     public bool IsLoggedIn => _currentId != 0;
+    public string Message => _message;
 
     public bool LoginById(int id)
     {
         var m = _accounts.GetById(id);
         if (m == null)
         {
-            message = $"Login failed: No account found with id {id}";
+            _message = $"Login failed: No account found with id {id}";
             return false;
         }
-
-        _currentId = (m == null ? 0 : m._Account);
-        message = $"{m.Name}, you are logged in! (ID: {m._Account}";
+        _currentId = m._Account;
+        _message = $"{m.Name}, you are logged in! (ID: {m._Account})";
         return true;
-        //stay logged in if not found
     }
 
     public bool LoginByName(string name)
     {
         if (string.IsNullOrWhiteSpace(name))
         {
-            message = "Login failed: Name is required";
+            _message = "Login failed: Name is required";
             return false;
         }
-
+        
         var m = _accounts.GetOrAdd(name);
-        if (m == null)
-        {
-            message = $"Login failed: No account found with the name {name}";
-            return false;
-        }
-
         _currentId = m._Account;
-        message = $"{m.Name} you are logged in! (ID:  {m._Account}";
+        _message = $"{m.Name}, you are logged in! (ID: {m._Account})";
         return true;
     }
 
     public void Logout()
     {
         _currentId = 0;
+        _message = "You are logged out.";
     }
 }
--- IAuthenticator.cs
+++ IAuthenticator.cs
@@ -4,6 +4,7 @@ public interface IAuthenticator
 {
     int CurrentId { get; }
     bool IsLoggedIn { get; }
+    string Message { get; }
     bool LoginById(int id);
     bool LoginByName(string name);
     void Logout();
